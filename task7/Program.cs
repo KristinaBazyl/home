32c0ab691483903c28be465f7ae0ef7870cd2e24
KristinaBazyl <kristi40890@yandex.ru>
2023-04-22 09:02:52 +0300

    task7

---
+++ task7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход целое число любой разрядности и на выходе показывает вторую цифру слева этого числа или говорит, что такой цифры нет. 
+
+Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num >9){
+  while (num > 99){
+    num = num / 10;
+  } 
+  Console.WriteLine(num % 10);
+}
+else{
+    Console.WriteLine("нет");
+}
