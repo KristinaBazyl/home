b5f61c6d3c4716538aa4af709b1edd79134f3071
KristinaBazyl <kristi40890@yandex.ru>
2023-04-30 21:40:53 +0300

    task23

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+int Palindrom(int num)
+{
+    int revers = 0;
+    while (num > 0)
+    {
+        revers = revers * 10 + num / 10;
+        num /= 10;
+    }
+    return revers;
+}
+
+
+
+Console.WriteLine("Введите число");
+int numPalindrom = Convert.ToUInt32(Console.ReadLine());
+if (numPalindrom < 0)
+{
+    Console.WriteLine($"{numPalindrom} не является палиндромом, введите число > 0");
+}
+if (numPalindrom == Palindrom(numPalindrom))
+{
+    Console.WriteLine($"{numPalindrom} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"{numPalindrom} не является палиндромом");
+}
---
+++ task23/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.WriteLine("Введите число");
+double num = Convert.ToDouble(Console.ReadLine());
+
+double firstNum = 1;
+double result = 0;
+while (firstNum <= num)
+{
+    result = Math.Pow(firstNum, 3);
+    Console.Write($" {result}");
+    firstNum++;
+}
