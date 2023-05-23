9ca79d6e64ceeaf36a287efc82feff05a32ceab3
KristinaBazyl <kristi40890@yandex.ru>
2023-05-23 14:03:03 +0300

    task34

--- task34/Program.cs
+++ task34/Program.cs
@@ -1,7 +1,10 @@
-﻿ void FillArray(int[] array)
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+//  которая покажет количество чётных чисел в массиве. 
+
+void FillArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(-100, 101);
+        array[i] = new Random().Next(100, 1000);
 }
 
 void PrintArray(int[] array)
@@ -10,3 +13,21 @@ void PrintArray(int[] array)
         Console.Write($"{item} ");
     System.Console.WriteLine();
 }
+
+
+int EvenNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+int[] Myarray = new int[7];
+FillArray(Myarray);
+PrintArray(Myarray);
+int res = EvenNum(Myarray);
+System.Console.WriteLine($"количество чётных чисел в массиве = {res}");
