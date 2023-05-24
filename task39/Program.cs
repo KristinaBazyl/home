56cd2f4f46f4d5c092cf725d4af488a368729f96
KristinaBazyl <kristi40890@yandex.ru>
2023-05-24 16:46:50 +0300

    task39

--- task39/Program.cs
+++ task39/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+﻿// 39 задача. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
 double [] FillArray(int size)
 {
@@ -18,9 +18,26 @@ void PrintArray(double [] array)
     System.Console.WriteLine();
 }
 
+void MaxMin(double [] array)
+{
+    double max = array[0];
+    double min = array[0];
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]> max) max= array [i];
+        if (array[i]< min) min= array [i];
+    }
+    System.Console.WriteLine($"максимальный элемент массива = {max}");
+    System.Console.WriteLine($"минимальный элемент массива = {min}");
+    System.Console.WriteLine($"разница между максимальным и минимальным элементом массива = {max - min}");
+
+}
+
 
 System.Console.WriteLine("введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 double [] array = FillArray(size);
 PrintArray(array);
+MaxMin(array);
 
