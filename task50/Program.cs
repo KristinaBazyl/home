b2632eb89705abc36f8d42adfbba72fc1b5bfe72
KristinaBazyl <kristi40890@yandex.ru>
2023-05-30 19:02:11 +0300

    task50

--- task50/Program.cs
+++ task50/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве,
+// и возвращает позицию этого элемента или же указание, что такого элемента нет.
+
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(-1, 1);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            System.Console.Write($"{array[i, j],3}    ");
+        System.Console.WriteLine();
+    }
+}
+
+string ElementArray(int[,] array, int number)
+{
+    string res = String.Empty;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == number)
+            {
+                res = ($"Позиция элемента {number}  = [{i},{j}]");
+                break;
+            }
+
+        }
+
+    }
+ if (res.Length>0) return res;
+ else return $"{number} такого числа в массиве нет";
+
+}
+
+
+
+Console.Clear();
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+FillArray(array);
+PrintArray(array);
+ElementArray(array, num);
+System.Console.WriteLine(ElementArray(array, num));
+
